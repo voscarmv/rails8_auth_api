5b7ee335c1b6662991d47a655d121ac45a9e2f0b
Oscar Mier <voscarmv@gmail.com>
2025-04-22 21:49:11 +0000

    Exception handling inside action controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
   include Authentication
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+  rescue_from ActionController::ParameterMissing, with: :handle_parameter_missing
+  rescue_from StandardError, with: :handle_internal_server_error
 
   # Used for JSON API response formatting
   def render_success(message:, data: {}, status: :ok)
@@ -18,21 +21,27 @@ class ApplicationController < ActionController::API
     }, status: status
   end
 
-  # Example: Add common error handling if needed
-  rescue_from ActiveRecord::RecordNotFound do |exception|
+  private
+
+  def handle_record_not_found(exception)
     render_error(
-      message: "Record not found.",
+      message: "Record not found",
       errors: { detail: exception.message },
       status: :not_found
     )
   end
 
-  rescue_from StandardError do |exception|
-    logger.error "[500] #{exception.class} - #{exception.message}"
-    logger.error exception.backtrace.join("\n") if Rails.env.development?
-
+  def handle_parameter_missing(exception)
     render_error(
-      message: "An unexpected error occurred.",
+      message: "Missing parameter",
+      errors: { detail: exception.message },
+      status: :bad_request
+    )
+  end
+
+  def handle_internal_server_error(exception)
+    render_error(
+      message: "Internal server error",
       errors: { detail: exception.message },
       status: :internal_server_error
     )
