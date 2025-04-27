123eae70d8febe77465344479aa0e25f0fcec4b5
Oscar Mier <voscarmv@gmail.com>
2025-04-27 22:16:30 +0000

    Remove unnecesary include

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :require_authentication
+  # before_action :require_authentication unnecessary because Auth is included in the app controller
   before_action :set_project, only: %i[ show update destroy ]
 
   # GET /projects
